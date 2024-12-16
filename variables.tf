2c77b01478ae5bb3263496080e9825637840f0ca
abirdoes <aidan.birdoes@neudesic.com>
2024-12-16 09:43:06 -0700

    Updated pipeline and variables

--- variables.tf
+++ variables.tf
@@ -1,4 +1,12 @@
 #General Variables
+
+variable "ARM_CLIENT_ID" {}
+
+variable "ARM_CLIENT_SECRET" {}
+
+variable "ARM_TENANT_ID" {}
+
+variable "ARM_SUBSCRIPTION_ID" {}
 variable "root_id" {
   type    = string
   default = "es"
